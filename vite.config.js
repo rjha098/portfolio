bb4b3181b68184f188441d1f8e06f87f51ca79b3
rjha098 <ramlaljha4@gmail.com>
2025-04-11 18:39:16 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css:{
-    modules:{
-      localsConvention:"camelCase"
-    }
-  }
-})
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+  base: "portfolio",
+});
