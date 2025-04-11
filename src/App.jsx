2e5d58362b6b5bc092b0bfe193a5289d7010cd58
rjha098 <ramlaljha4@gmail.com>
2025-04-11 20:26:01 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import styles from "./App.module.css";
 import About from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
-import Hero from "./components/Navbar/Hero/Hero";
+import {Hero} from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/NAvbar";
 import { Projects } from "./components/Projects/Projects";
 
